815b247702027e3d0c8eb7f20ac79c4cf683be29
ahmad-shami55 <ahmad.chami05@gmail.com>
2026-08-25 20:10:06 +0300

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/the-addict/',
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
-  }
+  base: '/the-addict/',
 })
